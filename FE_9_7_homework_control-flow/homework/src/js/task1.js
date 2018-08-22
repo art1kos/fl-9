c6e2bb0921edf4f096b01730a7ced2d896d0f1be
Artur Kostiuk <arturkos54@gmail.com>
2018-08-22 10:41:35 +0300

    updated

--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,26 +1,25 @@
-var login = prompt("Write your login", ""); 
+let login = prompt('Write your login', ''); 
 
 if(!login){
-	alert("Canceled!");
+	alert('Canceled!');
 }else if(login.length < 4){
-	alert("I don`t know any users havung name length less than 4 symbol");
-}else if(login == "User"){
-	var password = prompt("Write your password", "");
+	alert('I don`t know any users havung name length less than 4 symbol');
+}else if(login === 'User'){
+	let password = prompt('Write your password', '');
 	if(!password){
-		alert("Canceled");
-	}else if(password == 	"SuperUser"){
-		var now = new Date();
-		var h = now.getHours();
+		alert('Canceled');
+	}else if(password === 	'SuperUser'){
+		let now = new Date();
+		let h = now.getHours();
 		if(h < 20){
-			alert("Good day");
+			alert('Good day');
 		}else{
-			alert("Good evening");
+			alert('Good evening');
 		}
 
 	}else{
-		alert("Wrong password");
+		alert('Wrong password');
 	}
 }else{
-	alert("I don`t know you");
+	alert('I don`t know you');
 }
-
