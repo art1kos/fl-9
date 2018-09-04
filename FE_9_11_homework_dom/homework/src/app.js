276019febcfffd7ee0596442413c6b115b5b0b56
Artur Kostiuk <arturkos54@gmail.com>
2018-09-04 19:20:27 +0300

    homework_11
    dom

---
+++ FE_9_11_homework_dom/homework/src/app.js
@@ -0,0 +1,58 @@
+let rootNode = document.getElementById('root');
+
+let myNodelist = document.getElementsByTagName('LI');
+
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement('SPAN');
+  let txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+
+}
+let close = document.getElementsByClassName('close');
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = 'none';
+  }
+}
+
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+
+
+function newElement() {
+  let maxLegth = 10;
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('myInput').value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert('You must write something!');
+  }else if(myNodelist.length >= maxLegth){
+    alert('Maximum item per list are created');
+  }else{
+    document.getElementById('myUL').appendChild(li);
+  }
+  document.getElementById('myInput').value = '';
+
+  let span = document.createElement('SPAN');
+  let txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = 'none';
+    }
+  }
+}
